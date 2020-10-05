20f68d7b48ab0029d3909dc3460eed50867e732c
yanbingtao <yanbingtao963@Gmail.com>
2020-10-05 20:37:49 +0800

    Implement functions to parse user input

--- src/main/java/seedu/address/logic/commands/ArchiveCommand.java
+++ src/main/java/seedu/address/logic/commands/ArchiveCommand.java
@@ -4,11 +4,13 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.List;
 
+import javafx.scene.shape.Arc;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 /**
  * Archives a person identified using it's displayed index from the address book.
@@ -23,18 +25,22 @@ public class ArchiveCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Archive command not implemented yet";
+    public static final String MESSAGE_ARGUMENTS = "Archive Index: %1$d";
+
+    private final Index targetIndex;
 
 //    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 //
 //    private final Index targetIndex;
 //
-//    public DeleteCommand(Index targetIndex) {
-//        this.targetIndex = targetIndex;
-//    }
+    public ArchiveCommand(Index targetIndex) {
+        requireAllNonNull(targetIndex);
+        this.targetIndex = targetIndex;
+    }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return new CommandResult(MESSAGE_NOT_IMPLEMENTED_YET);
+        return new CommandResult(String.format(MESSAGE_ARGUMENTS, targetIndex.getOneBased()));
 //        requireNonNull(model);
 //        List<Person> lastShownList = model.getFilteredPersonList();
 //
@@ -47,10 +53,10 @@ public class ArchiveCommand extends Command {
 //        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
     }
 
-//    @Override
-//    public boolean equals(Object other) {
-//        return other == this // short circuit if same object
-//                || (other instanceof DeleteCommand // instanceof handles nulls
-//                && targetIndex.equals(((DeleteCommand) other).targetIndex)); // state check
-//    }
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ArchiveCommand // instanceof handles nulls
+                && targetIndex.equals(((ArchiveCommand) other).targetIndex)); // state check
+    }
 }
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -55,7 +55,7 @@ public class AddressBookParser {
             return new DeleteCommandParser().parse(arguments);
 
         case ArchiveCommand.COMMAND_WORD:
-            return new ArchiveCommand();
+            return new ArchiveCommandParser().parse(arguments);
 
         case ClearCommand.COMMAND_WORD:
             return new ClearCommand();
