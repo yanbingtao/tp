8f6b8419df4cebc83079b26fd98a4d6bef7a6989
Diwu-Yi <59159857+Diwu-Yi@users.noreply.github.com>
2020-10-07 23:03:51 +0800

    Reorganise import statements

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -22,8 +22,8 @@ import seedu.address.model.Drink;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
-import seedu.address.model.ingredient.Ingredient;
 import seedu.address.model.SalesBook;
+import seedu.address.model.ingredient.Ingredient;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
