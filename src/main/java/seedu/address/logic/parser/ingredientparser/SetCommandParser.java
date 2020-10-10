5bf92a06950af97db67d58d1ab03cba328565af8
WM71811 <55304760+WM71811@users.noreply.github.com>
2020-10-10 23:55:24 +0800

    Update SetCommandParser.java

--- src/main/java/seedu/address/logic/parser/ingredientparser/SetCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ingredientparser/SetCommandParser.java
@@ -10,10 +10,10 @@ import java.util.stream.Stream;
 import seedu.address.logic.commands.ingredientcommands.SetCommand;
 import seedu.address.logic.commands.ingredientcommands.SetCommand.SetIngredientDescriptor;
 import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.Prefix;
-import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ingredient.Ingredient;
 
