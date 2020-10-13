fe2d9510f56007f655677722f4559fae8e927d02
WM71811 <55304760+WM71811@users.noreply.github.com>
2020-10-13 22:26:49 +0800

    Update IngredientListCommand.java

--- src/main/java/seedu/address/logic/commands/ingredientcommands/IngredientListCommand.java
+++ src/main/java/seedu/address/logic/commands/ingredientcommands/IngredientListCommand.java
@@ -56,10 +56,7 @@ public class IngredientListCommand extends Command {
         if (!(other instanceof IngredientListCommand)) {
             return false;
         }
-        if (other instanceof IngredientListCommand) {
-            return true;
-        }
-        return false;
+        return true;
     }
 
 }
