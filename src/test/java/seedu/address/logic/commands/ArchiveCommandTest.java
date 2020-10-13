ba54d07eed1fd699480f06d1c2843b9f70cc91c4
yanbingtao <yanbingtao963@Gmail.com>
2020-10-13 12:08:12 +0800

    Add ArchiveCommandTest.java

---
+++ src/test/java/seedu/address/logic/commands/ArchiveCommandTest.java
@@ -0,0 +1,67 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.SalesBook;
+import seedu.address.model.UserPrefs;
+import seedu.address.testutil.TypicalIndexes;
+import seedu.address.testutil.TypicalPersons;
+
+public class ArchiveCommandTest {
+    private Model model = new ModelManager(TypicalPersons.getTypicalAddressBook(), new SalesBook(), new UserPrefs());
+
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        ArchiveCommand archiveCommand = new ArchiveCommand(outOfBoundIndex);
+
+        assertCommandFailure(archiveCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        showPersonAtIndex(model, TypicalIndexes.INDEX_FIRST_PERSON);
+
+        Index outOfBoundIndex = TypicalIndexes.INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of CouponStash list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        ArchiveCommand archiveCommand = new ArchiveCommand(outOfBoundIndex);
+
+        assertCommandFailure(archiveCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        ArchiveCommand archiveFirstCommand = new ArchiveCommand(INDEX_FIRST_PERSON);
+        ArchiveCommand archiveSecondCommand = new ArchiveCommand(INDEX_SECOND_PERSON);
+
+        // same object -> returns true
+        assertTrue(archiveFirstCommand.equals(archiveFirstCommand));
+
+        // same values -> returns true
+        ArchiveCommand archiveFirstCommandCopy = new ArchiveCommand(INDEX_FIRST_PERSON);
+        assertTrue(archiveFirstCommand.equals(archiveFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(archiveFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(archiveFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(archiveFirstCommand.equals(archiveSecondCommand));
+    }
+}
