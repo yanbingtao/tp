aa125b691a92ecef509b8f6271308c0add6c0ebb
yanbingtao <yanbingtao963@Gmail.com>
2020-10-13 17:14:55 +0800

    Remove unused imports in ArchiveAllCommand.java.

--- src/main/java/seedu/address/logic/commands/ArchiveAllCommand.java
+++ src/main/java/seedu/address/logic/commands/ArchiveAllCommand.java
@@ -1,16 +1,13 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
-import seedu.address.model.person.ArchiveStatus;
-import seedu.address.model.person.Person;
+import static java.util.Objects.requireNonNull;
 
 import java.util.List;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
 
 public class ArchiveAllCommand extends Command {
 
