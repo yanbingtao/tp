3012aa8c447df818f2d13d995380597d81511d3c
WM71811 <55304760+WM71811@users.noreply.github.com>
2020-10-14 20:07:24 +0800

    Update AddressBookParser.java

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -17,8 +17,8 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.SalesUpdateCommand;
-import seedu.address.logic.commands.ingredientcommands.IngredientListCommand;
 import seedu.address.logic.commands.SetCommand;
+import seedu.address.logic.commands.ingredientcommands.IngredientListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
