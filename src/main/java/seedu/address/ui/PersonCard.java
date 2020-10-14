acf1ab5a48aa025362c71ffe98164e072f4089a9
Persdre <persdre@gmail.com>
2020-10-14 23:19:44 +0800

    Add gui part for emergency

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -50,7 +50,7 @@ public class PersonCard extends UiPart<Region> {
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
-        //emergency.setText(person.getEmergency().value);
+        emergency.setText(person.getEmergency().value);
         address.setText(person.getAddress().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
